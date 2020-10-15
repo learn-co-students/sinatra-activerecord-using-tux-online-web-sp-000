c62eff77f442bdea0fe9c8d0d4883be87af04e57
kkirby16 <kkirby16@my.whitworth.edu>
2020-10-14 18:42:23 -0700

    Done.

--- db/migrate/20151117223101_create_users.rb
+++ db/migrate/20151117223101_create_users.rb
@@ -1,6 +1,6 @@
-class CreateUsers < ActiveRecord::Migration
+class CreateUsers < ActiveRecord::Migration[5.1]
   def change
-    create_table :users do |t|
+    create_table :users do |t| #ask if the user table is actually already created here because it looks like this was already migrated...?
       t.string :name
       t.string :email
       t.string :fav_icecream
